278c27691aa6be6c49ce6ebeb9c70dea6cf3532a
Sakti Dewantoro <saktyd@gmail.com>
2019-02-13 16:35:04 +0700

    delete unused file and used css javascript react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Content = styled.div`
+  text-align: center;
+`;
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'Sakti Dewantoro',
+      text: 'My Name'
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Content>
+        <h1>
+          Hello {this.state.text} {}
+          {this.state.name}
+        </h1>
+        <button>Click Me</button>
+      </Content>
     );
   }
 }
